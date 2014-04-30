8aa858559f1b908f6d2f472880fbe9c9a0cf9b4e
Jens Oettershagen <oettersh@ins.uni-bonn.de>
2014-04-30 17:53:25 +0200

    bloedsinn

---
+++ mquad/PlainMonteCarlo.hpp
@@ -0,0 +1,37 @@
+#ifndef PlainMonteCarlo.hpp
+#define PlainMonteCarlo.hpp
+
+
+
+
+/*
+ * get a function and maximum number of samplepoints	 
+ * 
+*/  
+
+
+template <class integrand, class pointgenerator>
+double applyQMCRule()
+
+class FullGrid : public QuasiMonteCarlo{
+  
+  
+  
+}
+
+double integrateMonteCarlo (blabla f,  unsigned dim, unsigned long maxSamples){
+  double expectation = 0;
+  
+  //init random number genrator
+  
+  for ( unsigned long i=0; i < maxSamples ; ++i){
+      
+    
+  }
+  
+  
+    return expectation;
+}
+
+#endif /* PlainMonteCarlo.hpp */
+
--- test/system/plain_mc.cpp
+++ test/system/plain_mc.cpp
@@ -4,10 +4,18 @@
 
 #include <mquad/PlainMonteCarlo.hpp>
 
-double f(const std::vector<double>& x){
+
+
+struct f{
+     unsigned dimension;
+  
+      double operator() const std::vector<double>& x){
 	double sum = 0;
 	for (unsigned j=0; j<x.size(); ++j)	sum += x[j];
 	return exp(sum);
+      }
+      
+      f() : dimension(3);  
 }
 
 
